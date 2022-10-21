cf4e85be253a686df0b6059bcf26c6d288521dfe
pyeong777 <sdp1123@naver.com>
2022-10-21 23:51:35 +0900

    add controlMenu component

---
+++ src/components/ControlMenu.js
@@ -0,0 +1,13 @@
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((item, idx) => (
+        <option key={idx} value={item.value}>
+          {item.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default ControlMenu;
