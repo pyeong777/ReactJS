bdfcce68f928f5a94ca62900bc9c30eebf71d43d
pyeong777 <sdp1123@naver.com>
2022-11-02 23:00:33 +0900

    add classname

--- src/components/EmotionItem.js
+++ src/components/EmotionItem.js
@@ -1,6 +1,6 @@
 const EmotionItem = ({ emotion_id, emotion_img, emotion_descript }) => {
   return (
-    <div>
+    <div className="EmotionItem">
       <img src={emotion_img} />
       <span>{emotion_descript}</span>
     </div>
