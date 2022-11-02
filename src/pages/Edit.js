5b26df6d699b79a59bc07a2c50e8786bfa43f8f1
pyeong777 <sdp1123@naver.com>
2022-11-03 00:11:45 +0900

    add edit page

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,9 +1,33 @@
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import DiaryEditor from '../components/DiaryEditor';
+import { DiaryStateContext } from './../App';
+
 const Edit = () => {
-    return (
-        <div>
-            Edit
-        </div>
-    );
+  const [originData, setOriginData] = useState();
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const diaryList = useContext(DiaryStateContext);
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id),
+      );
+
+      if (targetDiary) {
+        setOriginData(targetDiary);
+      } else {
+        navigate('/', { replace: true });
+      }
+    }
+  }, [id, diaryList]);
+
+  return (
+    <div>
+      {originData && <DiaryEditor isEdit={true} originData={originData} />}
+    </div>
+  );
 };
 
 export default Edit;
