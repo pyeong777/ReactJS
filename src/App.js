c46547cebf1864ba85c8e82fe4413e1da704931c
pyeong777 <sdp1123@naver.com>
2022-11-10 19:59:17 +0900

    add localstorage

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
-import React, { useReducer, useRef } from 'react';
+import React, { useEffect, useReducer, useRef } from 'react';
 
 const reducer = (state, action) => {
   let newState = [];
@@ -29,54 +29,25 @@ const reducer = (state, action) => {
     default:
       return state;
   }
+  localStorage.setItem('diary', JSON.stringify(newState));
   return newState;
 };
 
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
-//더미데이터
-const dummyData = [
-  {
-    id: 1,
-    emotion: 1,
-    content: '오늘의 일기 1번',
-    date: 1666359942362,
-  },
-  {
-    id: 2,
-    emotion: 2,
-    content: '오늘의 일기 2번',
-    date: 1666359942363,
-  },
-  {
-    id: 3,
-    emotion: 3,
-    content: '오늘의 일기 3번',
-    date: 1666359942364,
-  },
-  {
-    id: 4,
-    emotion: 4,
-    content: '오늘의 일기 4번',
-    date: 1666359942365,
-  },
-  {
-    id: 5,
-    emotion: 5,
-    content: '오늘의 일기 5번',
-    date: 1666359942366,
-  },
-  {
-    id: 6,
-    emotion: 6,
-    content: '오늘의 일기 6번',
-    date: 1667190002966,
-  },
-];
-
 function App() {
-  const [data, dispatch] = useReducer(reducer, dummyData);
+  const [data, dispatch] = useReducer(reducer, []);
+
+  useEffect(() => {
+    const localData = localStorage.getItem('diary');
+    if (localData) {
+      const diaryList = JSON.parse(localData).sort((a, b) => b.id - a.id);
+      dataId.current = diaryList[0].id + 1;
+
+      dispatch({ type: 'INIT', data: diaryList });
+    }
+  }, []);
 
   const dataId = useRef(0);
   //CREATE
